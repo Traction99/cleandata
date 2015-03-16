9176b0895fa38fe0283801e0d0e212cf76ffecb7
Traction99 <Traction99@users.noreply.github.com>
2015-03-16 10:30:58 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,34 @@
+## Download the file and put the file in the data folder. 
+## unzip the filed in the folderUCI HAR Dataset. Get the list of the files.
+
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip")
-
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
-
 path_rf <- file.path("./data" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
 files
 
+##
+##Values of Varible Activity consist of data from “Y_train.txt” and “Y_test.txt”
+##values of Varible Subject consist of data from “subject_train.txt” and subject_test.txt"
+##Values of Varibles Features consist of data from “X_train.txt” and “X_test.txt”
+##Names of Varibles Features come from “features.txt”
+##levels of Varible Activity come from “activity_labels.txt”
+
+
 dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
 dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
-
 dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
 dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
-
 dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
 dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
 
 str(dataActivityTest)
-
 str(dataActivityTrain)
-
 str(dataSubjectTrain)
-
 str(dataSubjectTest)
-
 str(dataFeaturesTest)
-
 str(dataFeaturesTrain)
 
 dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
@@ -53,6 +55,8 @@ Data<-subset(Data,select=selectedNames)
 
 str(Data)
 
+#######################################
+
 activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
 
 head(Data$activity,30)
@@ -63,17 +67,19 @@ names(Data)<-gsub("Acc", "Accelerometer", names(Data))
 names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
 names(Data)<-gsub("Mag", "Magnitude", names(Data))
 names(Data)<-gsub("BodyBody", "Body", names(Data))
-
 names(Data)
-
-
 library(plyr);
 Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
 write.table(Data2, file = "tidydata.txt",row.name=FALSE)
 
-#library(knitr)
-#knit2html("codebook.Rmd");
+#########################
+library(memisc)
+Datafinal <- data.set(Data2)
+codebook(Datafinal)
+Write(codebook(Datafinal), file="codebook.md")
+
+
 
 
 
